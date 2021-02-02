41c04e300f7c19956dd10c9f180781a3f3f50ac2
Apisit Ritreungroj <apisit_lon@hotmail.com>
2021-02-02 13:53:04 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import Table from "./components/Table";
+
+const MY_ID = 620610820;
+
 function App() {
+  const [ counter, setCounter ] = useState(MY_ID);
+
   const persons = [
     {
       name: "Bob",
@@ -9,6 +16,11 @@ function App() {
       name: "Alice",
       gender: "male",
       age: "20"
+    },
+    {
+      name: "Apisit Ritreungroj 620610820",
+      gender: "male",
+      age: "20"
     }
   ];
   return (
@@ -16,27 +28,14 @@ function App() {
       <h3 class="title is-3">Person List</h3>
 
       {/* Convert me to a component! */}
-      <table class="table is-bordered mb-3">
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Age</th>
-          </tr>
-          <tr>
-            <td>Bob</td>
-            <td>male</td>
-            <td>50</td>
-          </tr>
-        </tbody>
-      </table>
+      {persons.map((person) => <Table key={Math.random()} {...person} />)}
 
       {/* Code me please! */}
       <h3 class="title is-3">ID Counter</h3>
-      <p>YOUR ID HERE</p>
-      <button>-</button>
-      <button>reset</button>
-      <button>+</button>
+      <p>{counter}</p>
+      <button onClick={() => setCounter(counter - 1)} >-</button>
+      <button onClick={() => setCounter(MY_ID)} >reset</button>
+      <button onClick={() => setCounter(counter + 1)} >+</button>
     </div>
   );
 }
